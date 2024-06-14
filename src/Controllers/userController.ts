d7bc016b80b17b5f70d034a451fdd13cdb24506b
akanksha <akankshapatel315@gmail.com>
2024-06-14 09:41:23 +0530

    test

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -12,7 +12,7 @@ const register = async (req:Request, res:Response) => {
     res.status(201).json({ message: 'User registered successfully', user });
   } catch (error:any) {
     res.status(500).json({ message: error.message });
-  }
+  } 
 };
 
 const login = async (req:Request, res:Response) => {
