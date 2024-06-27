41b452f8da160df7c46c8d94b02cb73fdf42c7f8
akanksha <akankshapatel315@gmail.com>
2024-06-27 13:52:24 +0530

    import changes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-const expressServer= require("express");
+const express = require("express");
 const dbConnect = require("../src/config/dbConnection")
 const cors= require("cors");
-const mainRouterr= require("./Routers/index")
-const stripee = require('stripe')(process.env.API_KEY);
+const mainRouter= require("./Routers/index")
+import {Response,Request}  from "express"
 
-const app = expressServer();
+const app = express();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cors());
 
+app.use(express.json());
 
-app.use(mainRouterr)
+app.use(mainRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
